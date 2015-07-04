516a1861326baa6667c03cfb796fe2f5e7a87d20
plasmashadow <plasmashadowx@gmail.com>
2015-07-04 21:34:18 +0530

    adding api base

--- stackexchange/api/_api_base.py
+++ stackexchange/api/_api_base.py
@@ -2,6 +2,7 @@ __author__ = 'plasmashadow'
 
 import six
 import urllib
+import json
 import requests
 
 
@@ -25,11 +26,24 @@ def _transform_to_object(item):
 def _construct_url(route, version, **kwargs):
 
     version = version or "2.2"
-    host = "https://api.stackexchange.com/"+version+"/"+route+"?"+urllib.urlencode(**kwargs)
+    host = "https://api.stackexchange.com/"+version+"/"+route+"?"+urllib.urlencode(kwargs)
     return host
 
+def parse(func):
+    def inner(url, *args, **kwargs):
+        data = func(url, *args, **kwargs)
+        data = json.loads(data)
+        _results = []
+        for item in data["items"]:
+            _results.append(_transform_to_object(item))
+        return _results
+    return inner
+
+@parse
 def _get(url, *args, **kwargs):
     return requests.get(url).content
-
+@parse
 def _post(url, *args, **kwargs):
     return requests.post(url).content
+
+
